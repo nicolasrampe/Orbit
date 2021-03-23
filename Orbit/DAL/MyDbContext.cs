829c9c6d797474b91c83fb2113a84c9751b6231a
Nicolás F. Ramírez Peña <colachoramirez5@gmail.com>
2021-03-23 16:44:09 -0500

    First

---
+++ Orbit/DAL/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orbit.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Orbit.DAL
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=sample.db3;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ Orbit/Model/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orbit.Model
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string Username { get; set; }
+        [StringLength(20)]
+        public string Firstname { get; set; }
+        [StringLength(20)]
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        [StringLength(20)]
+        public string Career { get; set; }
+
+    }
+}
